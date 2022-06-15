267deabd5f6bc75782cc8ef7641c2f9617049722
andrewrocc <andreyrocc@gmail.com>
2022-06-15 16:01:46 +0300

    nameless commit
    Signed-off-by: andrewrocc <andreyrocc@gmail.com>

--- src/main/java/by/academy/homework1/Main.java
+++ src/main/java/by/academy/homework1/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        task_1();
+        task_1();
 //        task_2();
 //        task_3();
 //        task_4();
