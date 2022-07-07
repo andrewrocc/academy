385d18ae8afb301a52fedf7cda04c9d0428e80e1
andrewrocc <andreyrocc@gmail.com>
2022-07-07 16:36:44 +0300

    benchmarking of "my" list of int primitives

--- src/main/java/by/academy/benchmark/BenchmarkCodeTest.java
+++ src/main/java/by/academy/benchmark/BenchmarkCodeTest.java
@@ -1,53 +1,86 @@
 package by.academy.benchmark;
 
+import by.academy.homework3.services.DynamicIntArray;
+import by.academy.experiment.Quicksort;
 import org.openjdk.jmh.annotations.*;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Thread)
 public class BenchmarkCodeTest {
 
-    private int[] patternsDateFormat;
+    @Param({ "10", "1000", "10000", "100000", "1000000" })
+    private int array_size;
+
+    private DynamicIntArray dynamic_array;
+
+    private Integer[] array;
+
     @Setup
     public void prepare() {
-        patternsDateFormat = new int[1000];
-        for (int i = 0; i < patternsDateFormat.length; i++) {
-            patternsDateFormat[i] = i + 1;
+        Random r = new Random();
+        array = new Integer[array_size];
+        dynamic_array = new DynamicIntArray(array_size);
+        for (int i = 0; i < array.length; i++) {
+            int random_value = r.nextInt(Integer.MAX_VALUE);
+            array[i] = random_value;
+            dynamic_array.add(random_value);
         }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @Measurement(iterations = 5, timeUnit = TimeUnit.SECONDS)
-    @Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
-    @Fork(value = 2)
+    @Measurement(iterations = 3, timeUnit = TimeUnit.SECONDS)
+    @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+    @Fork(value = 1)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Timeout(time = 10, timeUnit = TimeUnit.MILLISECONDS)
-    public String byteShift() {
-        int result_mul = 1;
-        for (int i = 0; i < patternsDateFormat.length; i++) {
-            result_mul *= patternsDateFormat[i];
-        }
+    public String linkedListSort() {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>(Arrays.asList(array));
+        Collections.sort(linkedList);
 
-        return String.valueOf(result_mul);
+        return String.valueOf(linkedList.indexOf(9));
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @Measurement(iterations = 5, timeUnit = TimeUnit.SECONDS)
-    @Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
-    @Fork(value = 2)
+    @Measurement(iterations = 3, timeUnit = TimeUnit.SECONDS)
+    @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+    @Fork(value = 1)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Timeout(time = 10, timeUnit = TimeUnit.MILLISECONDS)
-    public String booleanState() {
-        int result_mul = 1;
-        for (var e : patternsDateFormat) {
-            result_mul *= e;
-        }
+    public String arrayListSort() {
+        ArrayList<Integer> naiveArrayList = new ArrayList<Integer>(Arrays.asList(array));
+        Collections.sort(naiveArrayList);
 
-        return String.valueOf(result_mul);
+        return String.valueOf(naiveArrayList.indexOf(9));
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @Measurement(iterations = 3, timeUnit = TimeUnit.SECONDS)
+    @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+    @Fork(value = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Timeout(time = 10, timeUnit = TimeUnit.MILLISECONDS)
+    public String rawArray() {
+        Arrays.sort(array);
+
+        return String.valueOf(array[9]);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @Measurement(iterations = 3, timeUnit = TimeUnit.SECONDS)
+    @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+    @Fork(value = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Timeout(time = 10, timeUnit = TimeUnit.MILLISECONDS)
+    public String dynamicIntArray() {
+        Quicksort qsorter = new Quicksort();
+        qsorter.sort(dynamic_array.getStorage());
+
+        return String.valueOf(array[9]);
     }
 }
